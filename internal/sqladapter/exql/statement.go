27273c99d975e8e4158cdcda172a8cfa242d38f8
swh <swhbox@foxmail.com>
2016-08-26 16:27:45 +0800

    update

--- internal/sqladapter/exql/statement.go
+++ internal/sqladapter/exql/statement.go
@@ -2,6 +2,7 @@ package exql
 
 import (
 	"reflect"
+	"strings"
 
 	"github.com/webx-top/db/internal/cache"
 )
@@ -109,8 +110,8 @@ func (s *Statement) Compile(layout *Template) (compiled string) {
 		panic("Unknown template type.")
 	}
 
+	compiled = strings.TrimSpace(compiled)
 	layout.Write(s, compiled)
-
 	return compiled
 }
 
