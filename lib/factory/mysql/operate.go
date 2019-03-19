c0955fb37759919e938d3d147aca0d1924fce726
shen_wen_hui <shenwenhui@zuzuche.com>
2019-03-19 15:33:33 +0800

    update

--- lib/factory/mysql/operate.go
+++ lib/factory/mysql/operate.go
@@ -57,3 +57,48 @@ func FieldExists(linkID int, tableName string, fieldName string) (bool, error) {
 	}
 	return param.String(recv.String).Int64() > 0, err
 }
+
+// MoveToNewTable 移动数据到新表
+func MoveToNewTable(linkID int, srcTableName string, destTableName string, condition string, src2DestFieldMapping ...map[string]string) (int64, error) {
+	sqlStr := "INSERT INTO `" + destTableName + "`"
+	var srcFields, destFields string
+	if len(src2DestFieldMapping) > 0 {
+		var sep string
+		for srcField, destField := range src2DestFieldMapping[0] {
+			srcFields += sep + "`" + srcField + "`"
+			destFields += sep + "`" + destField + "`"
+			sep = ","
+		}
+		destFields = `(` + destFields + `)`
+	}
+	sqlStr += destFields
+	if len(srcFields) < 1 {
+		srcFields = `*`
+	}
+	sqlStr += ` SELECT ` + srcFields + ` FROM ` + "`" + srcTableName + "`"
+	if len(condition) > 0 {
+		sqlStr += ` WHERE ` + condition
+	}
+	db := factory.NewParam(linkID).DB()
+	result, err := db.Exec(sqlStr)
+	if err != nil {
+		return 0, err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return affected, err
+	}
+	if affected > 0 {
+		sqlStr := "DELETE FROM `" + srcTableName + "`"
+		if len(condition) > 0 {
+			sqlStr += ` WHERE ` + condition
+		}
+		db := factory.NewParam(linkID).DB()
+		result, err := db.Exec(sqlStr)
+		if err != nil {
+			return affected, err
+		}
+		affected, err = result.RowsAffected()
+	}
+	return affected, err
+}
