30b5bfbcf88d4c8698398d7edbf3c0e6bfbc52d5
Hank Shen <swhbox@foxmail.com>
2019-04-05 12:06:06 +0800

    generator: refactory & added AsMap

---
+++ _tools/generator/config.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"path/filepath"
+	"strings"
+)
+
+func parseFlag() {
+	flag.StringVar(&configFile, `c`, ``, `-c conf.yaml`)
+
+	//DBSettings
+	flag.StringVar(&cfg.Username, `u`, `root`, `-u user`)
+	flag.StringVar(&cfg.Password, `p`, ``, `-p password`)
+	flag.StringVar(&cfg.Host, `h`, `localhost`, `-h host`)
+	flag.StringVar(&cfg.Engine, `e`, `mysql`, `-e engine`)
+	flag.StringVar(&cfg.Database, `d`, `blog`, `-d database`)
+	flag.StringVar(&cfg.Prefix, `pre`, ``, `-pre prefix`)
+
+	//DBSchema
+	flag.StringVar(&cfg.SchemaConfig.ImportPath, `import`, ``, `-import github.com/webx-top/project/app/dbschema`)
+	flag.StringVar(&cfg.SchemaConfig.PackageName, `pkg`, `dbschema`, `-pkg packageName`)
+	flag.StringVar(&cfg.SchemaConfig.SaveDir, `o`, `dbschema`, `-o targetDir`)
+
+	//Model
+	flag.StringVar(&cfg.ModelConfig.BaseName, `mbase`, `Base`, `-mbase Base`)
+	flag.StringVar(&cfg.ModelConfig.ImportPath, `mimport`, ``, `-mimport github.com/webx-top/project/app/model`)
+	flag.StringVar(&cfg.ModelConfig.SaveDir, `mo`, ``, `-mo targetDir`)
+	flag.StringVar(&cfg.ModelConfig.PackageName, `mpkg`, `model`, `-mpkg packageName`)
+
+	//Postgres schema
+	flag.StringVar(&cfg.Schema, `schema`, `public`, `-schema schemaName`)
+
+	//Time
+	flag.StringVar(&autoTime, `autoTime`, `{"update":{"*":["updated"]},"insert":{"*":["created"]}}`, `-autoTime <json-data>`)
+	flag.Parse()
+}
+
+var cfg = &config{
+	SchemaConfig: &SchemaConfig{},
+	ModelConfig:  &ModelConfig{},
+}
+var configFile, autoTime string
+
+type SchemaConfig struct {
+	SaveDir     string `json:"saveDir"`
+	ImportPath  string `json:"importPath"`
+	PackageName string `json:"packageName"`
+}
+
+type ModelConfig struct {
+	SaveDir     string `json:"saveDir"`
+	ImportPath  string `json:"importPath"`
+	PackageName string `json:"packageName"`
+	BaseName    string `json:"baseName"`
+}
+
+type config struct {
+	Username       string          `json:"username"`
+	Password       string          `json:"password"`
+	Host           string          `json:"host"`
+	Engine         string          `json:"engine"`
+	Database       string          `json:"database"`
+	Prefix         string          `json:"prefix"`
+	SchemaConfig   *SchemaConfig   `json:"schemaConfig"`
+	ModelConfig    *ModelConfig    `json:"modelConfig"`
+	Schema         string          `json:"schema"`
+	AutoTimeFields *AutoTimeFields `json:"autoTime"`
+}
+
+func (cfg *config) Check() {
+	if len(cfg.ModelConfig.SaveDir) > 0 {
+		cfg.ModelConfig.SaveDir = strings.TrimSuffix(cfg.ModelConfig.SaveDir, `/`)
+		cfg.ModelConfig.SaveDir = strings.TrimSuffix(cfg.ModelConfig.SaveDir, `\`)
+		if len(cfg.ModelConfig.PackageName) == 0 {
+			cfg.ModelConfig.PackageName = filepath.Base(cfg.ModelConfig.SaveDir)
+			switch cfg.ModelConfig.PackageName {
+			case `.`, `/`, `\`:
+				cfg.ModelConfig.PackageName = `model`
+			}
+		}
+	}
+
+	if cfg.AutoTimeFields == nil && len(autoTime) > 0 {
+		cfg.AutoTimeFields = &AutoTimeFields{}
+		cfg.AutoTimeFields.Parse(autoTime)
+	}
+}
+
+type AutoTimeFields struct {
+	//Update update操作时，某个字段自动设置为当前时间（map的键和值分别为表名称和字段名称。当表名称设置为“*”时，代表所有表中的这个字段）
+	Update map[string][]string `json:"update"`
+
+	//Insert insert操作时，某个字段自动设置为当前时间（map的键和值分别为表名称和字段名称。当表名称设置为“*”时，代表所有表中的这个字段）
+	Insert map[string][]string `json:"insert"`
+}
+
+func (a *AutoTimeFields) Parse(autoTime string) {
+	// JSON
+	if (autoTime)[0] == '{' {
+		err := json.Unmarshal([]byte(autoTime), a)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	// update(*:updated)/insert(*:created) 括号内的格式：<表1>:<字段1>,<字段2>,<...字段N>;<表2>:<字段1>,<字段2>,<...字段N>
+	a.Update = make(map[string][]string)
+	a.Insert = make(map[string][]string)
+	for _, par := range strings.Split(autoTime, `/`) {
+		par = strings.TrimSpace(par)
+		switch {
+		case strings.HasPrefix(par, `update(`):
+			a.parseUpdateTime(par)
+
+		case strings.HasPrefix(par, `insert(`):
+			a.parseInsertTime(par)
+		}
+	}
+}
+
+func (a *AutoTimeFields) parseUpdateTime(par string) {
+	par = strings.TrimPrefix(par, `update(`)
+	par = strings.TrimSuffix(par, `)`)
+	for _, item := range strings.Split(par, `;`) {
+		t := strings.SplitN(item, `:`, 2)
+		if len(t) < 2 {
+			continue
+		}
+		t[0] = strings.TrimSpace(t[0])
+		t[1] = strings.TrimSpace(t[1])
+		if len(t[0]) == 0 || len(t[1]) == 0 {
+			continue
+		}
+		if _, ok := a.Update[t[0]]; !ok {
+			a.Update[t[0]] = []string{}
+		}
+		for _, field := range strings.Split(t[1], `,`) {
+			field = strings.TrimSpace(field)
+			if len(field) == 0 {
+				continue
+			}
+			a.Update[t[0]] = append(a.Update[t[0]], field)
+		}
+	}
+}
+
+func (a *AutoTimeFields) parseInsertTime(par string) {
+	par = strings.TrimPrefix(par, `insert(`)
+	par = strings.TrimSuffix(par, `)`)
+	for _, item := range strings.Split(par, `;`) {
+		t := strings.SplitN(item, `:`, 2)
+		if len(t) < 2 {
+			continue
+		}
+		t[0] = strings.TrimSpace(t[0])
+		t[1] = strings.TrimSpace(t[1])
+		if len(t[0]) == 0 || len(t[1]) == 0 {
+			continue
+		}
+		if _, ok := a.Insert[t[0]]; !ok {
+			a.Insert[t[0]] = []string{}
+		}
+		for _, field := range strings.Split(t[1], `,`) {
+			field = strings.TrimSpace(field)
+			if len(field) == 0 {
+				continue
+			}
+			a.Insert[t[0]] = append(a.Insert[t[0]], field)
+		}
+	}
+}
---
+++ _tools/generator/utils.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/webx-top/db/lib/factory"
+	"github.com/webx-top/db/lib/sqlbuilder"
+)
+
+func TableToStructName(tableName string, prefix string) string {
+	if len(prefix) > 0 {
+		tableName = strings.TrimPrefix(tableName, prefix)
+	}
+	return factory.ToCamleCase(tableName)
+}
+
+func ZeroValue(typ string) string {
+	switch typ {
+	case `uint`, `int`, `uint32`, `int32`, `int64`, `uint64`:
+		return `0`
+	case `float32`, `float64`:
+		return `0.0`
+	case `bool`:
+		return `false`
+	case `string`:
+		return "``"
+	case `byte[]`:
+		return `nil`
+	case `time.Time`:
+		return `time.Time{}`
+	default:
+		panic(`undefined zero value for ` + typ)
+	}
+}
+
+func DataType(fieldInfo *factory.FieldInfo) string {
+	switch fieldInfo.DataType {
+	case `int`, `tinyint`, `smallint`, `mediumint`:
+		if fieldInfo.Unsigned {
+			return `uint`
+		}
+		return `int`
+	case `bigint`:
+		if fieldInfo.Unsigned {
+			return `uint64`
+		}
+		return `int64`
+	case `decimal`, `double`:
+		return `float64`
+	case `float`:
+		return `float32`
+	case `bit`, `binary`, `varbinary`, `tinyblob`, `blob`, `mediumblob`, `longblob`: //二进制
+		return `byte[]`
+	case `geometry`, `point`, `linestring`, `polygon`, `multipoint`, `multilinestring`, `multipolygon`, `geometrycollection`: //几何图形
+		return `byte[]`
+
+	//postgreSQL
+	case `boolean`:
+		return `bool`
+	case `oid`:
+		if fieldInfo.Unsigned {
+			return `uint64`
+		}
+		return `int64`
+
+	default:
+		return `string`
+	}
+}
+
+func GetTableFields(engine string, d sqlbuilder.Database, tableName string) ([]string, map[string]factory.FieldInfo, []string) {
+	switch engine {
+	case "mymysql", "mysql":
+		fallthrough
+	default:
+		return getMySQLTableFields(d, tableName)
+	}
+}
