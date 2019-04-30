340c915583355c27cb02dc23d044aea93738e336
shen_wen_hui <shenwenhui@zuzuche.com>
2019-04-30 18:05:01 +0800

    update

--- _tools/generator/main.go
+++ _tools/generator/main.go
@@ -226,11 +226,11 @@ func main() {
 	dataContent := strings.Replace(fmt.Sprintf(`factory.Fields=%#v`+"\n", allFields), `map[string]map[string]factory.FieldInfo`, `map[string]map[string]*factory.FieldInfo`, -1)
 	dataContent = strings.Replace(dataContent, `map[string]factory.FieldInfo`, ``, -1)
 	dataContent = strings.Replace(dataContent, `:factory.FieldInfo`, `:`, -1)
+	for structName, modelInstancer := range modelInstancers {
+		dataContent += "\n\tfactory.Models[`" + structName + "`] = " + modelInstancer
+	}
 	content = strings.Replace(content, `{{packageName}}`, cfg.SchemaConfig.PackageName, -1)
 	content = strings.Replace(content, `{{initCode}}`, dataContent, -1)
-	for structName, modelInstancer := range modelInstancers {
-		content += "\nfactory.Models[" + structName + "] = " + modelInstancer
-	}
 	saveAs := filepath.Join(cfg.SchemaConfig.SaveDir, `init`) + `.go`
 	file, err := os.Create(saveAs)
 	if err == nil {
