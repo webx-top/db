ae28f00230bafb825ba448a9a909ab2315aa0feb
Hank Shen <swhbox@foxmail.com>
2019-10-03 23:52:34 +0800

    update

---
+++ lib/factory/info_event_test.go
@@ -0,0 +1,42 @@
+package factory_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/webx-top/db/_tools/generator/dbschema"
+	"github.com/webx-top/db/lib/factory"
+)
+
+func TestEvent(t *testing.T) {
+	dbi := dbschema.DBI
+	buf := new(bytes.Buffer)
+
+	// 定义事件
+	dbi.EventOn(`creating`, func(m factory.Model) error {
+		buf.WriteString(`creating.`)
+		println(`creating.`)
+		return nil
+	}, `config`)
+	dbi.EventOn(`created`, func(m factory.Model) error {
+		buf.WriteString(`created.`)
+		println(`created.`)
+		return nil
+	}, `config`)
+
+	// 调用事件
+	m := &dbschema.Config{}
+	dbi.EventFire(`creating`, m, nil)
+	assert.Equal(
+		t,
+		`creating.`,
+		buf.String(),
+	)
+	dbi.EventFire(`created`, m, nil)
+	assert.Equal(
+		t,
+		`creating.created.`,
+		buf.String(),
+	)
+}
