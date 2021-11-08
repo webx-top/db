4b537aaf9fb45092795a9ab6a4c23ae084dcc2b4
Hank Shen <swh@admpub.com>
2021-11-08 11:09:13 +0800

    update

--- lib/factory/param.go
+++ lib/factory/param.go
@@ -538,6 +538,12 @@ func (p *Param) SQLBuilder() sqlbuilder.SQLBuilder {
 	return p.T().SQLBuilder(p)
 }
 
+func (p *Param) DoFunc(fn func() error) func(*Param) error {
+	return func(*Param) error {
+		return fn()
+	}
+}
+
 // Read ==========================
 
 // Cached query support cache
