e8660f9141a67319480768f9763531a692634eeb
Hank Shen <swh@admpub.com>
2023-08-26 19:01:51 +0800

    update

--- logger_test.go
+++ logger_test.go
@@ -4,11 +4,27 @@ import (
 	"testing"
 )
 
+type myInt int
+
 func TestBuildSQL(t *testing.T) {
 	query := "select * from `a` where a=? and b=?"
-	expected := "select * from `a` where a='1' and b='c'"
+	expected := "select * from `a` where a=1 and b='c'"
 	actual := BuildSQL(query, 1, `c`)
 	if expected != actual {
-		t.Fatal(actual,"!=",expected)
+		t.Fatal(actual, "!=", expected)
+	}
+	myI := myInt(1)
+	actual = BuildSQL(query, myI, `c`)
+	if expected != actual {
+		t.Fatal(actual, "!=", expected)
+	}
+	actual = BuildSQL(query, &myI, `c`)
+	if expected != actual {
+		t.Fatal(actual, "!=", expected)
+	}
+	expected = "select * from `a` where a='1' and b='c'"
+	actual = BuildSQL(query, `1`, `c`)
+	if expected != actual {
+		t.Fatal(actual, "!=", expected)
 	}
 }
