c12a42c8d16d5974724035e175ce8a67e5f8661d
Hank Shen <swh@admpub.com>
2024-09-16 16:20:26 +0800

    update

--- mysql/stubs_test.go
+++ mysql/stubs_test.go
@@ -1,3 +1,4 @@
+//go:build !generated
 // +build !generated
 
 package mysql
@@ -6,6 +7,7 @@ import (
 	"log"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/webx-top/db/lib/sqlbuilder"
 )
 
@@ -13,6 +15,32 @@ func mustOpen() sqlbuilder.Database {
 	return nil
 }
 
-func TestMain(*testing.M) {
+func _mustOpen() sqlbuilder.Database {
+	sess, err := Open(settings)
+	if err != nil {
+		panic(err.Error())
+	}
+	return sess
+}
+
+func TestMain(m *testing.M) {
+	m.Run()
 	log.Fatal(`Tests use generated code and a custom database, please use "make test".`)
+}
+
+func TestJSON(t *testing.T) {
+	sess := _mustOpen()
+	defer sess.Close()
+
+	type _MyType struct {
+		ID                        int64  `db:"id,omitempty"`
+		JSONArray                 string `db:"json_array"`
+		AutoCustomJSONObjectArray string `db:"auto_custom_json_object_array"`
+	}
+	var actual _MyType
+	err := sess.Collection("my_types").Find(1).One(&actual)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), actual.ID)
+	assert.Equal(t, `[1, 2, 3, 4]`, actual.JSONArray)
+	assert.Equal(t, `[{"name": "Hello", "value": 0}, {"name": "World", "value": 0}]`, actual.AutoCustomJSONObjectArray)
 }
